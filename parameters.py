89119544966f9323fca8447d91ac2b1a74a8e783
Charles <charles.dognin@ensae.Fr>
2017-12-04 17:10:35 +0100

    change parameters agents to 5

--- parameters.py
+++ parameters.py
@@ -1,6 +1,6 @@
 parameters = {"batch_size": 32,
               "optimizer": "Adam",
-              "nb_agents": 4,
+              "nb_agents": 5,
               "env_name": 'SpaceInvaders-v0',
               "input_shape": (210, 160, 3),
               "agent_parameters": {"epsilon": 1.1}
